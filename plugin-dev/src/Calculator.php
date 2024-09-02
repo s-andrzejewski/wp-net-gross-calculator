fed2185acea1bbdd963cbc0bc99e862a321a5dbb
s-andrzejewski <s.andrzejewski.7@gmail.com>
2024-09-02 03:52:41 +0200

    feat(Calculator): validate backend

--- plugin-dev/src/Calculator.php
+++ plugin-dev/src/Calculator.php
@@ -24,34 +24,90 @@ class Calculator
         // 'currency' => string,
         // 'vatRate' => double (eg. 0.23) or null,
 
-        $grossAmount = 0.0;
+        self::validate($params);
 
-        // TODO: Check if can be better solution for Exceptions
-        //* Validation
-        // TODO: Enclose in a seperate method
-        if (gettype($params['netAmount']) != 'double') {
-            throw new \Exception("Bad format of 'netAmount' var.");
-        }
-        if (gettype($params['vatRate']) != 'double') {
-            throw new \Exception("Bad format of 'vatRate' var.");
-        }
-        if ($params['netAmount'] < 0.00) {
-            throw new \Exception("'netAmount' cannot be negative.");
-        }
-        if ($params['vatRate'] < 0.00) {
-            throw new \Exception("'vatRate' cannot be negative.");
-        }
+        $netAmount = (float)$params['netAmount'];
+        $vatRate = (float)$params['vatRate'];
 
         $vatAmount = 0.0;
-        if ($params['vatRate'] > 0.00) {
-            $vatAmount = $params['netAmount'] * $params['vatRate'];
+        if ($vatRate > 0.00) {
+            $vatAmount = $netAmount * $vatRate;
         }
 
-        $grossAmount = $params['netAmount'] + $vatAmount;
+        $grossAmount = $netAmount + (float)$vatAmount;
+
+        self::setTwoPointsAfterDot($grossAmount);
+        self::setTwoPointsAfterDot($vatAmount);
 
         return [
-            'grossAmount' => $grossAmount,
-            'vatAmount' => $vatAmount
+            'grossAmount' => (string)$grossAmount,
+            'vatAmount' => (string)$vatAmount
         ];
+    }
+
+    private static function validate(&$params)
+    {
+        $errors = [];
+
+        //* productName
+        if (
+            empty($params['productName'])
+            || !is_string($params['productName'])
+        ) {
+            $errors[] = 'Product name must be a non-empty string.';
+        }
+
+        //* netAmount
+        $params['netAmount'] = self::validateAndFormatNumber($params['netAmount'], 'Net amount', $errors);
+
+        //* currency
+        if (empty($params['currency']) || !is_string($params['currency'])) {
+            $errors[] = 'Currency must be a non-empty string.';
+        }
+
+        //* vatRate
+        $params['vatRate'] = self::validateAndFormatNumber($params['vatRate'], 'VAT rate', $errors);
+
+        if (!empty($errors)) {
+            throw new \Exception(implode(' ', $errors));
+        }
+    }
+
+    private static function validateAndFormatNumber($value, $fieldName, &$errors)
+    {
+        if (!is_string($value) || trim($value) === '') {
+            $errors[] = "{$fieldName} must be a non-empty string.";
+            return null;
+        }
+
+        $value = str_replace(',', '.', str_replace(' ', '', $value));
+
+        $numberValue = filter_var($value, FILTER_VALIDATE_FLOAT);
+        if ($numberValue === false) {
+            $errors[] = "{$fieldName} must be a valid number.";
+            return null;
+        }
+
+        self::setTwoPointsAfterDot($value);
+
+        return $value;
+    }
+
+    private static function setTwoPointsAfterDot(&$value) {
+        $value = (string)$value;
+
+        if (!strpos($value, '.')) {
+            $value .= '.00';
+        } else {
+            $parts = explode('.', $value);
+            if (strlen($parts[1]) === 1) {
+                $value .= '0';
+            }
+            if (strlen($parts[1]) > 2) {
+                $value = number_format((float)$value, 2, '.', '');
+            }
+        }
+
+        $value = (float)$value;
     }
 }
